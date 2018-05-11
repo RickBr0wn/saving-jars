aa633d36e2e1ee58dcc898ef1efbffcc8ba5b43a
RickBr0wn <ricky.brown.00@gmail.com>
2018-05-11 23:55:41 +0100

    Added CSS styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
+// Components
+import List from './Components/List'
+
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      stateTest: 'test'
+    }
+  }
+
   render() {
+    const styles = {
+      wrapper: {
+        width: '400px',
+        maxWidth: '400px',
+        backgroundColor: '#eee',
+        textAlign: 'center',
+        marginTop: '-16px',
+        paddingTop: '20px',
+        paddingBottom: '20px'
+      }
+    }
+
     return (
-      <div>
-        
+      <div style={styles.wrapper}>
+        <p>SAVING JAR</p>
+        <List stateTest={this.state.stateTest} />
       </div>
     );
   }
