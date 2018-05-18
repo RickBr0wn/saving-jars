d955609206ee04d7b2baa9122618b00b72adde21
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-18 14:14:17 +0100

    Material-UI styling

--- src/Components/JarList.js
+++ src/Components/JarList.js
@@ -2,8 +2,19 @@ import React from "react"
 
 //Components
 import NewJar from './NewJar'
-import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import { withStyles } from '@material-ui/core/styles';
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
 
 class JarList extends React.Component {
   constructor() {
@@ -100,42 +111,55 @@ class JarList extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="test">
         <ul>
           {
             this.state.jars.map((jar, index) => {
               return(
                 <div className="jar" key={index}>
-									<TextField 	id={jar.name}
-															label={jar.name}
-															placeholder="Amount to Save"
-															onChange={event => this.handleChangeAt(event, index)} 
-															value={jar.amountToBeAdded} />
-									<li>{jar.name}</li>
-									<li>{jar.amount}</li>
-									<li>{jar.amountToBeAdded}</li>
-
-                  <form onSubmit={event => this.handleSubmit(event, index)}>
-                    <label>Money To Add To Jar
-                    <input  type="text"
-                            value={jar.amountToBeAdded}
-                            onChange={event => this.handleChangeAt(event, index)} />
-                    </label>
-                    <input type="submit" value="submit" />
-                  </form>
-									<button onClick={() => this.handleRemoveAt(index)}
-													type="submit">REMOVE JAR</button>
+                  <div className="title">{jar.name}</div>
+                  <div className="amount">£{jar.amount}</div>
+                  <div className="controls">
+                    <TextField 	id={jar.name}
+                                label="Amount to save"
+                                placeholder="£"
+                                onChange={event => this.handleChangeAt(event, index)} 
+                                value={jar.amountToBeAdded} />
+                    <IconButton onClick={event => this.handleSubmit(event, index)}
+                                className="butt" 
+                            // color="primary"
+                            // variant="fab" 
+                            // color="primary" 
+                            aria-label="add" 
+                            // size="small"
+                            // className={classes.button}
+                            >
+                      <AddIcon />
+                    </IconButton>
+                    <IconButton onClick={() => this.handleRemoveAt(index)}
+                                aria-label="Delete"
+                                className="butt"
+                                type="submit">
+                      <DeleteIcon />
+                    </IconButton>
+                  </div>
+                  
                 </div>
+                
               ) 
             })
           }
         </ul>
-				<NewJar pendingJar={this.state.pendingJar}
-								handleNewJarSubmit={this.handleNewJarSubmit}
-								handleNewJarNameChangeAt={this.handleNewJarNameChangeAt} />
+        <br />
+        <div className="row">
+          <p>CREATE A NEW JAR</p>
+          <NewJar pendingJar={this.state.pendingJar}
+                  handleNewJarSubmit={this.handleNewJarSubmit}
+                  handleNewJarNameChangeAt={this.handleNewJarNameChangeAt} />
+        </div>
       </div>
     )
   }
 }
 
-export default JarList
+export default withStyles(styles)(JarList)
--- src/Components/NewJar.js
+++ src/Components/NewJar.js
@@ -1,16 +1,88 @@
 import React from 'react'
 
-const NewJar = props  => {
-  return(
-    <form onSubmit={event => props.handleNewJarSubmit(event)}>
-      <label>Name of New Jar
-      <input  type="text"
-              value={props.pendingJar}
-              onChange={props.handleNewJarNameChangeAt} />
-      </label>
-      <input type="submit" value="submit" />
-    </form>                    
-  )
+// Components
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class NewJar extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      open: false,
+    }
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render(){
+    const { classes } = this.props;
+    return(
+      // <form onSubmit={event => props.handleNewJarSubmit(event)}>
+      //   <label>Name of New Jar
+      //   <input  type="text"
+      //           value={props.pendingJar}
+      //           onChange={props.handleNewJarNameChangeAt} />
+      //   </label>
+      //   <input type="submit" value="submit" />
+      // </form>        
+      <div>
+        <Button variant="fab" 
+                color="primary" 
+                aria-label="add" 
+                className={classes.button}
+                onClick={this.handleClickOpen}>
+          <AddIcon />
+        </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Create A New Savings Jar</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please enter the name of your new Savings Jar
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Name of Jar"
+              onChange={this.props.handleNewJarNameChangeAt}
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.props.handleNewJarSubmit} color="primary">
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    )
+  }
 }
 
-export default NewJar
+export default withStyles(styles)(NewJar)
